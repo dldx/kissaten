0b3942b1c6eb7c721e80c483481136de9abf34c4
Durand D'souza <durand1@gmail.com>
2025-12-30 03:06:26 +0000

    feat: Add dynamic theme color meta tag, update libsql client import, and temporarily disable view transitions.

--- frontend/src/lib/server/database/index.ts
+++ frontend/src/lib/server/database/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from 'drizzle-orm/libsql'
-import { createClient } from '@libsql/client'
+import { createClient } from '@libsql/client/node'
 import { env } from '$env/dynamic/private'
 import * as schema from './schema'
 
